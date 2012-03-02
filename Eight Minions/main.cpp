8b6ff8facb01cb3ccb3dfcc922e3b4c1272fe8d5
Jeromy Johnson <Jeromyj@gmail.com>
2012-03-02 12:20:39 -0800

    adding new classes

--- Eight Minions/main.cpp
+++ Eight Minions/main.cpp
@@ -2,6 +2,22 @@
 
 int main(void){
 	
+	printf("Host or Connect to host? (host-1, connect-2)\n>");
+	int c = 0;
+	cin >> c;
+	if(c == 1)
+	{
+		unsigned int port;
+		game_host server;
+		printf("enter port to host on:");
+		cin >> port;
+		server.setPort(port);
+		//do other server things, namely...
+		server.run();
+	}
+	if(c == 2)
+	{
 
+	}
 	return 0;
 }
--- Eight Minions/resources.h
+++ Eight Minions/resources.h
@@ -1,1 +1,8 @@
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
 #include <iostream>
+#include "game_host.h"
+#include "client.h"
+
+#endif
