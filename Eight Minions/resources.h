2404de31f1542999c137bd947a51113a4c6d0992
Jeromy Johnson <Jeromyj@gmail.com>
2012-03-03 13:39:40 -0800

    added socket library

--- Eight Minions/resources.h
+++ Eight Minions/resources.h
@@ -3,13 +3,14 @@
 
 #include <iostream>
 #include <string>
+#include <winsock2.h>
 #include "SDL.h"
 
-/*using std::cout;
+using std::cout;
 using std::cin;
 using std::endl;
-using std::string;*/
+using std::string;
 
-using namespace std; //Oh really?
+//using namespace std; //Oh really? - it was just to test something, forgot to change back
 
 #endif
