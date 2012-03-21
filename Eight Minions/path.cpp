0de8f18f4da3296136bca52cadfa48f38f4794ac
Jeromy Johnson <Jeromyj@gmail.com>
2012-03-21 00:17:41 -0700

    trying to make pathfinding work

--- Eight Minions/path.cpp
+++ Eight Minions/path.cpp
@@ -13,7 +13,16 @@ Path::Path(int x, int y)
 
 int Path::numPaths(coord c)
 {
-
+	int n = 0;
+	if(c.x >= 0 && Nodemap[c.x - 1][c.y] == 0)
+		n++;
+	if(c.y >= 0 && Nodemap[c.x][c.y - 1] == 0)
+		n++;
+	if(c.x < mapsize.x && Nodemap[c.x + 1][c.y] == 0)
+		n++;
+	if(c.y	< mapsize.y && Nodemap[c.x][c.y + 1] == 0)
+		n++;
+	return n;
 }
 
 int Path::genPath()
@@ -21,10 +30,40 @@ int Path::genPath()
 	//insert super fast efficient algorithm for finding the fastest route from the start to the finish
 	//1 = impassable, 0 = passable
 	coord cur = goal;
+	int paths, int dx, dy;
 	p.push(cur);
-	while(cur != start)
+	while(cur != start && !p.empty())
 	{
+		paths = numPaths(cur);
+		if(paths == 0)
+		{
+			return -1;
+		}
+		if(paths == 1)
+		{
+			Nodemap[c.x][c.y] = 1;
+			cur = p.top();
+			p.pop();
+		}
+		//move towards goal in some way.
+		dx = goal.x - cur.x;
+		dy = goal.y - cur.y;
+		if(abs(dx) > abs(dy))
+		{
+			if(dx == abs(dx))
+			{
+				
+			}
+		}
 		
+
+
+	}
+	if(cur != start)
+		return -1;
+	else
+	{
+		//add path to some sort of list...
 	}
 	return 1;
 }
@@ -37,8 +76,8 @@ void Path::setNodeMap()
 
 void Path::setStart(int x, int y)
 {
-start.x = x;
-start.y = y;
+	start.x = x;
+	start.y = y;
 }
 
 void Path::setGoal(int x, int y)
