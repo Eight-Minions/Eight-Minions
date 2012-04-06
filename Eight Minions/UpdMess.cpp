0e3037d027a5dfeb9fa0706d03a2f011ba5aab51
Travis Lane <travis.lane.osx@gmail.com>
2012-04-05 21:08:24 -0700

    Modified UpdMess. Allowed for different size creep updates.

--- Eight Minions/UpdMess.cpp
+++ Eight Minions/UpdMess.cpp
@@ -4,6 +4,13 @@ UpdMess::UpdMess()
 }
 UpdMess::UpdMess(int p, int t, ...)
 {
+
+	/*
+		Creep:			UpdMess(Player[1], CREEP, CreepID[4], X[4], Y[4], Health[5], Type[2], Level[3]);	// For creep creation and upgrades
+		Creep:			UpdMess(Player[1], CREEP, CreepID[4], X[4], Y[4], Health[5]);						// For position updates
+		Tower:			UpdMess(Player[1], TOWER, TowerID[4], X[2], Y[2], TowerType[2]);
+		Tower Attack:	UpdMess(Player[1], TOWERATTACK, AttackerID[4], AttackedID[4], AttackType[2]);
+	*/
 	int var;
 	char buff[8];
 	va_list v1;	
@@ -38,11 +45,13 @@ UpdMess::UpdMess(int p, int t, ...)
 			}
 		}
 	}
-	else if(t == TOWER || t == CREEP)
+	else if(t== CREEP)
 	{
-		for(int i = 0; i < 4; i++)
+		for(int i = 0; i < 6; i++)
 		{
 			var = va_arg(v1, int);
+			if(i >= 4 && var == NULL)
+				break; // The update type of a Creep is only 4 vars.
 			if(i == 0 || i == 1 || i == 2)
 			{
 				for(int n = 3 - (int)floor(log10((double)var)); n > 0; n--)
@@ -53,25 +62,32 @@ UpdMess::UpdMess(int p, int t, ...)
 			}
 			else if(i == 3)
 			{
-				if(t == CREEP)
+				for(int n = 4 - (int)floor(log10((double)var)); n > 0; n--)
 				{
-					for(int n = 4 - (int)floor(log10((double)var)); n > 0; n--)
-					{
-						messText += '0';
-					}
-					messText += itoa(var,buff,10);
+					messText += '0';
 				}
-				if(t == TOWER)
-				{
-
-					if(var < 10)
-						messText += '0';
-					messText += itoa(var,buff,10);
-				}
+				messText += itoa(var,buff,10);
 			}
 			else
 			{
 				// ERROR
+			}
+		}
+	}
+	else if(t == TOWER){
+		for (int i = 0; i < 4; i++){
+			var = va_arg(v1, int);
+			if(i == 0){
+				for(int n = 3 - (int)floor(log10((double)var)); n > 0; n--)
+				{
+					messText += '0';
+				}
+				messText += itoa(var,buff,10);
+			}
+			else{
+				if(var < 10)
+					messText += '0';
+				messText += itoa(var,buff,10);
 			}
 		}
 	}
@@ -81,8 +97,11 @@ UpdMess::UpdMess(int p, int t, ...)
 	va_end(v1);
 }
 UpdMess::UpdMess(string m){
+	/*
 	for(int i = 0; i < MAX_NUM_VAL; i++) //Couldnt we just do a val.resize(MAX_NUM_VAL) here?
 		val.push_back(0);
+	*/
+	val.resize(MAX_NUM_VAL);
 	type = m[0] - '0';
 	p = m[1] - '0';
 	if(type == CREEP)
@@ -96,10 +115,10 @@ UpdMess::UpdMess(string m){
 		// HEALTH
 		val[2] = 10000 * (m[14] - '0') + 1000 * (m[15] - '0') + 100 * (m[16] - '0') + 10 * (m[17] - '0') + (m[18] - '0');
 		if (m.length() == 24){
-		// TYPE
-		val[3] = 10 * (m[19] - '0') + (m[20] - '0');
-		// LEVEL
-		val[4] = 100 * (m[21] - '0') + 10 * (m[22] - '0') + (m[23] - '0');
+			// TYPE
+			val[3] = 10 * (m[19] - '0') + (m[20] - '0');
+			// LEVEL
+			val[4] = 100 * (m[21] - '0') + 10 * (m[22] - '0') + (m[23] - '0');
 		}
 	}
 	else if(type == TOWER)
@@ -107,11 +126,11 @@ UpdMess::UpdMess(string m){
 		// ID
 		id1 = 1000 * (m[2] - '0') + 100 * (m[3] - '0') + 10 * (m[4] - '0') + (m[5] - '0');
 		// X
-		val[0] = 1000 * (m[6] - '0') + 100 * (m[7] - '0') + 10 * (m[8] - '0') + (m[9] - '0');
+		val[0] = 10 * (m[6] - '0') + (m[7] - '0');
 		// Y
-		val[1] = 1000 * (m[10] - '0') + 100 * (m[11] - '0') + 10 * (m[12] - '0') + (m[13] - '0');
+		val[1] = 10 * (m[8] - '0') + (m[9] - '0');
 		// TYPE
-		val[2] = 10 * (m[14] - '0') + (m[15] - '0');
+		val[2] = 10 * (m[10] - '0') + (m[11] - '0');
 	}
 	else if(type == TOWERATTACK)
 	{
