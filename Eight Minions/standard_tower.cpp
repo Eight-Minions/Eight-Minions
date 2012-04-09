67c1f0ab007d633e21aef5ec8e5ba100009c2a5b
Travis Lane <travis.lane.osx@gmail.com>
2012-04-09 13:40:50 -0700

    An attempt at an attack, commented out due to issues.

--- Eight Minions/standard_tower.cpp
+++ Eight Minions/standard_tower.cpp
@@ -12,10 +12,43 @@ Standard_Tower::Standard_Tower(int l, int t, int set_x, int set_y, game_host *nM
 void Standard_Tower::damageClosestCreep(int radius)
 {
 	/*
-	for(int i = 0; i < this->creepList.size(); i++)
+	int x_high =	getX() + radius;
+	int x_low =		getX() - radius;
+	int y_high =	getY() + radius;
+	int y_low =		getY() - radius;
+	int x_closest = -1;
+	int y_closest = -1;
+	int closestCreep = -1;
+	double distanceClosest = 0;
+	for(int i = 0; i < this->manager->creepList.maxIterator; i++)
 	{
-		// if creep is in range
-			creepList[i].damage(amount);
+		if(manager->creepList.checkForObjectWithID(i))
+		{
+			int x_cur = manager->creepList.getObjectWithID(i)->getX(); // Convert from pixels to squares
+			int y_cur = manager->creepList.getObjectWithID(i)->getY(); // Convert form pixels to squares
+			if(x_high > x_cur && x_low < x_cur)
+			{
+				if(y_high > y_cur && y_low < y_cur)
+				{
+					if(closestCreep != -1)
+					{
+						double distanceCur = sqrt(pow(abs(x_cur - getX()), 2) + pow(abs(y_cur - getY()),2));	
+						if(distanceCur < distanceClosest)
+						{
+							closestCreep = i;
+							x_closest = x_cur;
+							y_closest = y_cur;
+						}
+					}
+					else
+					{
+						closestCreep = i;
+						x_closest = x_cur;
+						y_closest = y_cur;
+					}
+				}
+			}
+		}
 	}
 	*/
 }
