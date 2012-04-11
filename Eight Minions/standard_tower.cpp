16f3feefc9a4af0f2b8641311812375b139b2076
unknown <Travis@Travis-System76.(none)>
2012-04-11 12:45:23 -0700

    More standard tower work

--- Eight Minions/standard_tower.cpp
+++ Eight Minions/standard_tower.cpp
@@ -1,14 +1,18 @@
 #include "standard_tower.h"
 
-Standard_Tower::Standard_Tower() : tower(){
+Standard_Tower::Standard_Tower() : tower()
+{
 }
-Standard_Tower::Standard_Tower(int l, int p, int t, int set_x, int set_y) : tower(STANDARDTOWERSTARTLEVEL, p, -1, set_x, set_y){
+Standard_Tower::Standard_Tower(int l, int p, int t, int set_x, int set_y) : tower(STANDARDTOWERSTARTLEVEL, p, -1, set_x, set_y)
+{
 }
-Standard_Tower::Standard_Tower(int l, int p, int t, int set_x, int set_y, game_host *nManager) : tower(STANDARDTOWERSTARTLEVEL, p, -1, set_x, set_y){
+Standard_Tower::Standard_Tower(int l, int p, int t, int set_x, int set_y, game_host *nManager) : tower(STANDARDTOWERSTARTLEVEL, p, -1, set_x, set_y)
+{
 	manager = nManager;
 }
-void Standard_Tower::damageClosestCreep(double radius)
+void Standard_Tower::chooseClosestCreep(double radius)
 {
+	
 	double distanceClosest = radius;
 	cListNode<creep*> *cur = NULL;
 	creep *closestCreep = NULL;
@@ -16,7 +20,7 @@ void Standard_Tower::damageClosestCreep(double radius)
 	cur = this->manager->getCreepList()->getStart();
 	while(cur != NULL)
 	{
-		if(cur->getData()->getPlayer() == this->getPlayer())
+		if(cur->getData()->getPlayer() != this->getPlayer())
 		{
 			double currentDistance = sqrt(pow((double)(((cur->getData()->getX() - BOARD_X_OFFSET)/GRID_SIZE)-((this->getX() - BOARD_X_OFFSET)/GRID_SIZE)), 2) +
 				pow((double)(((cur->getData()->getY() - BOARD_Y_OFFSET)/GRID_SIZE)-((this->getY() - BOARD_Y_OFFSET)/GRID_SIZE)), 2));
@@ -28,11 +32,10 @@ void Standard_Tower::damageClosestCreep(double radius)
 		}
 		cur = cur->getNext();
 	}
-	if(closestCreep != NULL){
-		closestCreep->damage(damageValue);
-	}
+	if(closestCreep != NULL)
+		chosenCreeps.push(closestCreep);
 }
-void Standard_Tower::damageClosestCreepToPosition(double radius, location position)
+void Standard_Tower::chooseClosestCreepToPosition(double radius, location position)
 {
 	double distanceClosest = radius;
 	cListNode<creep*> *cur = NULL;
@@ -41,64 +44,130 @@ void Standard_Tower::damageClosestCreepToPosition(double radius, location positi
 	cur = this->manager->getCreepList()->getStart();
 	while(cur != NULL)
 	{
-		double currentDistance = sqrt(pow((double)(((cur->getData()->getX() - BOARD_X_OFFSET)/GRID_SIZE)-((position.getX() - BOARD_X_OFFSET)/GRID_SIZE)), 2) +
-			pow((double)(((position.getY() - BOARD_Y_OFFSET)/GRID_SIZE)-((this->getY() - BOARD_Y_OFFSET)/GRID_SIZE)), 2));
-		if(currentDistance < distanceClosest)
+		if(cur->getData()->getPlayer() != this->getPlayer())
 		{
-			closestCreep = cur->getData();
-			distanceClosest = currentDistance;
+			double currentDistance = sqrt(pow((double)(((cur->getData()->getX() - BOARD_X_OFFSET)/GRID_SIZE)-((position.getX() - BOARD_X_OFFSET)/GRID_SIZE)), 2) +
+				pow((double)(((position.getY() - BOARD_Y_OFFSET)/GRID_SIZE)-((this->getY() - BOARD_Y_OFFSET)/GRID_SIZE)), 2));
+			if(currentDistance < distanceClosest)
+			{
+				closestCreep = cur->getData();
+				distanceClosest = currentDistance;
+			}
 		}
 		cur = cur->getNext();
 	}
-	if(closestCreep != NULL){
-		closestCreep->damage(damageValue);
-	}
+	if(closestCreep != NULL)
+		chosenCreeps.push(closestCreep);
 }
-void Standard_Tower::damageNeighbors(double radius)
+void Standard_Tower::chooseNeighbors(double radius)
 {
-	list<creep*> inRadius;
 	cListNode<creep*> *cur = this->manager->getCreepList()->getStart();
 
 	while(cur != NULL)
 	{
-		double currentDistance = sqrt(pow((double)(((cur->getData()->getX() - BOARD_X_OFFSET)/GRID_SIZE)-((this->getX() - BOARD_X_OFFSET)/GRID_SIZE)), 2) +
-			pow((double)(((cur->getData()->getY() - BOARD_Y_OFFSET)/GRID_SIZE)-((this->getY() - BOARD_Y_OFFSET)/GRID_SIZE)), 2));
-		if(currentDistance < radius)
+		if(cur->getData()->getPlayer() != this->getPlayer())
 		{
-			inRadius.push_back(cur->getData());
+			double currentDistance = sqrt(pow((double)(((cur->getData()->getX() - BOARD_X_OFFSET)/GRID_SIZE)-((this->getX() - BOARD_X_OFFSET)/GRID_SIZE)), 2) +
+				pow((double)(((cur->getData()->getY() - BOARD_Y_OFFSET)/GRID_SIZE)-((this->getY() - BOARD_Y_OFFSET)/GRID_SIZE)), 2));
+			if(currentDistance < radius)
+			{
+				chosenCreeps.push(cur->getData());
+			}
 		}
 		cur = cur->getNext();
 	}
-	while(inRadius.size() > 0)
-	{
-		if(inRadius.back() != NULL)
-		{
-			inRadius.back()->damage(damageValue);
-			inRadius.pop_back();
-		}
-	}
 }
-void Standard_Tower::damageNeighborsNearPosition(double radius, location position)
+void Standard_Tower::chooseNeighborsNearPosition(double radius, location position)
 {
-	list<creep*> inRadius;
 	cListNode<creep*> *cur = this->manager->getCreepList()->getStart();
 
 	while(cur != NULL)
 	{
-		double currentDistance = sqrt(pow((double)(((cur->getData()->getX() - BOARD_X_OFFSET)/GRID_SIZE)-((position.getX() - BOARD_X_OFFSET)/GRID_SIZE)), 2) +
-			pow((double)(((cur->getData()->getY() - BOARD_Y_OFFSET)/GRID_SIZE)-((position.getY() - BOARD_Y_OFFSET)/GRID_SIZE)), 2));
-		if(currentDistance < radius)
+		if(cur->getData()->getPlayer() != this->getPlayer())
 		{
-			inRadius.push_back(cur->getData());
+			double currentDistance = sqrt(pow((double)(((cur->getData()->getX() - BOARD_X_OFFSET)/GRID_SIZE)-((position.getX() - BOARD_X_OFFSET)/GRID_SIZE)), 2) +
+				pow((double)(((cur->getData()->getY() - BOARD_Y_OFFSET)/GRID_SIZE)-((position.getY() - BOARD_Y_OFFSET)/GRID_SIZE)), 2));
+			if(currentDistance < radius)
+			{
+				chosenCreeps.push(cur->getData());
+			}
 		}
 		cur = cur->getNext();
 	}
-	while(inRadius.size() > 0)
+}
+
+/*
+	choses creeps if the cool down is 0
+	returns true if something was chosen
+	returns false if nothing was chosen or the tick is not up.
+*/
+bool Standard_Tower::choose()
+{
+	if(coolDownTick <= 0)
 	{
-		if(inRadius.back() != NULL)
+		if(attackStrategy == ATTACKCLOSESTTOTOWER)
 		{
-			inRadius.back()->damage(damageValue);
-			inRadius.pop_back();
+			if(attackType == AREAOFEFFECT)
+			{
+				this->chooseNeighbors(range);
+			}
+			else if(attackType == ATTACKONECREEP)
+			{
+				this->chooseClosestCreep(range);
+			}
+			else
+				return false;
 		}
+		else if(attackStrategy == ATTACKCLOSESTTOBASE)
+		{
+			if(attackType == AREAOFEFFECT)
+			{
+
+			}
+			else if(attackType == ATTACKONECREEP)
+			{
+
+			}
+			else
+				return false;
+		}
+		else
+			return false;
+		if(chosenCreeps.size() > 0)
+		{
+			coolDownTick = coolDownDuration;
+			return true;  // Stuff was chosen
+		}
+		else
+			return false; //Nothing was chosen. 
+	}
+	else
+	{
+		coolDownTick--;   //Next iteration
+		return false;
+	}
+}
+/*
+	Does damage to creeps if the tick is 0
+	Returns true if it should damage the creeps;
+	Retruns false if it needs to go to the next iteration
+*/
+bool Standard_Tower::doDamage()
+{
+	if(attackTick <= 0)
+	{
+		while(chosenCreeps.size() > 0)
+		{
+			if(chosenCreeps.front() != NULL) // Does the creep still exist?
+				chosenCreeps.front()->damage(damageValue);
+			chosenCreeps.pop();
+		}
+		attackTick = attackDuration;
+		return true;
+	}
+	else
+	{
+		attackTick--;
+		return false;
 	}
 }
--- Eight Minions/standard_tower.h
+++ Eight Minions/standard_tower.h
@@ -4,23 +4,39 @@
 #include "tower.h"
 #include "game_host.h"
 
+
+#define ATTACKCLOSESTTOTOWER	1
+#define ATTACKCLOSESTTOBASE		2
+
+#define AREAOFEFFECT			1
+#define ATTACKONECREEP			2
+
 class Standard_Tower : public tower
 {
 public:
 	Standard_Tower();
 	Standard_Tower(int l, int p, int t, int set_x, int set_y);
 	Standard_Tower(int l, int p, int t, int set_x, int set_y, game_host *nManager);
-	void damageClosestCreep(double radius);
-	void damageClosestCreepToPosition(double radius, location position);
-	void damageNeighbors(double radius);
-	void damageNeighborsNearPosition(double radius, location position);
+	void chooseClosestCreep(double radius);
+	void chooseClosestCreepToPosition(double radius, location position);
+	void chooseNeighbors(double radius);
+	void chooseNeighborsNearPosition(double radius, location position);
+	bool choose();
+	bool doDamage();
 private:
 	game_host *manager;
+	queue<creep*> chosenCreeps;
+
 	int damageValue;
-	double range;
-	int attackSpeed;
 	int attackType;
+	double range;
+
+	int coolDownTick;
+	int coolDownDuration;
+
+	int attackTick;
 	int attackStrategy;
+	int attackDuration;
 };
 
 #endif
