17bd20caab37fba5146862b51d11965beec551b2
Jeromy Johnson <Jeromyj@gmail.com>
2012-04-12 22:54:29 -0700

    attacks should show now

--- Eight Minions/client_net.cpp
+++ Eight Minions/client_net.cpp
@@ -141,8 +141,7 @@ int client::performUpdate(string upd)
 
 	}
 	else if(updateType == TOWERATTACK){
-
-
+		attacks.push_back(new attackAnim(update.getVal(0),update.getVal(1),0,40,update.getVal(2)));
 	}
 	else{
 		return 0;
