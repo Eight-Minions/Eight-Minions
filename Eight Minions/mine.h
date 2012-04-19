2ce85bb31743f4d25cd416af3f5c41cb018d0a1b
Travis Lane <travis.lane.osx@gmail.com>
2012-04-19 00:36:36 -0700

    Added a mine class to fill out

---
+++ Eight Minions/mine.h
@@ -0,0 +1,23 @@
+#ifndef MINE_H
+#define MINE_H
+
+#include "resources.h"
+#include "tower.h"
+#include "standard_tower.h"
+
+class Mine : public Standard_Tower
+{
+public:
+	Mine();
+	Mine(int level, int player, int type, int set_x, int set_y);
+	Mine(int level, int player, int type, int set_x, int set_y, game_host *nManager);
+	~Mine();
+private:
+	game_host *manager;
+	queue<int> chosenCreeps;
+
+	int cost;
+	int range;
+
+};
+#endif
