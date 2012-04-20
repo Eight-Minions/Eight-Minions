89fcacddffb7d63657e0f439fa4d75ec96ee5937
Travis Lane <travis.lane.osx@gmail.com>
2012-04-20 02:04:10 -0700

    Filling out map a little

--- Eight Minions/map.cpp
+++ Eight Minions/map.cpp
@@ -2,4 +2,28 @@
 
 Map::Map()
 {
+	mapname = "No Map";
+}
+
+string Map::getMapName()
+{
+	return mapname;
+}
+void Map::load(string filename)
+{
+	ifstream input;
+	input.open(filename.c_str(),ifstream::in); 
+	if(input!= NULL)
+	{
+
+	}
+	else
+	{
+		cout << "File not found, unable to load" << endl;	
+	}
+	input.close();
+}
+bool verify()
+{
+	return true;
 }
--- Eight Minions/map.h
+++ Eight Minions/map.h
@@ -1,10 +1,19 @@
 #ifndef MAP_H
 #define MAP_H
+
+#include "resources.h"
+#include "game_host.h"
+
 class Map
 {
 public:
 	Map();
+	void load(string filename);
+	bool verify();
+	string getMapName();
 
-
+private:
+	string mapname;
+	vector<coord*> objectLocations;
 };
 #endif
